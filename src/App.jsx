65a2dd38627acc15d12a3b4ddeb2cb09b86a21f9
MdAlAmin212104 <mdalamin212104@gmail.com>
2024-03-09 15:25:31 +0600

    error solved

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,30 @@ import { useEffect, useState } from 'react';
 import './App.css'
 import Button from './component/Button'
 import Friends from './component/Friends';
-import Posts from './component/post/posts';
-
 function App() {
-  // const [count, setCount] = useState(0);
-  // const [user, setUser] = useState([]);
-  // useEffect(() =>{
-  //   fetch('https://jsonplaceholder.typicode.com/users')
-  //   .then (res => res.json())
-  //   .then (data => setUser(data));
+  const [count, setCount] = useState(0);
+  const [user, setUser] = useState([]);
+  useEffect(() =>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then (res => res.json())
+    .then (data => setUser(data));
     
-  // }, []);
+  }, []);
   
   return (
     
     <>
     <h1>this is react project</h1>
-    <Posts></Posts>
+    
     {/* <Person name = 'Rahim'/>
     <Person name = 'Karim'/>
     <Person name = 'Ali'/>
     <Person name = 'Rashad'/>
     
   <Button name ="mobile"/> */}
-    {/* <button onClick={()=> setCount(count + 1)}>Click {count}</button>
+    <button onClick={()=> setCount(count + 1)}>Click {count}</button>
   <p>{user.length} </p>
-  <Friends/> */}
+  <Friends/>
       
     </>
   )
